22f8025399403a12a5301392fc0ef65ff90d2aa4
Tetiana <t.pasichnyk@ukr.net>
2020-04-03 01:12:48 +0300

    m<0

--- Return.js
+++ Return.js
@@ -1,8 +1,10 @@
 function largestOfFour(arr) {
-var longest=0;
-let arrayarray=[];
+
+var arrayarray=[];
 for (i=0; i<arr.length; i+=1) {
     var newarray = arr[i];
+    // var longest=0;
+    var longest=newarray[1];
     for (m=0; m<newarray.length; m+=1){
 
         if(newarray[m]>longest){
@@ -12,9 +14,9 @@ for (i=0; i<arr.length; i+=1) {
 
     }
     arrayarray.push(longest);
-    // console.log(longest);
+
 }
 return arrayarray;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
